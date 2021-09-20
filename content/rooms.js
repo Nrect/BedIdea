167bc7ff1731be24c43c4b61ae7bf4d71360f97f
Лисицын Александр <bossstartov57@gmail.com>
2021-09-20 16:54:51 +0200

    room translate

--- content/rooms.js
+++ content/rooms.js
@@ -86,12 +86,12 @@ export const roomsData = [
       // avif: require('@/assets/images/room6/1.jpg?resize&format=avif')
     },
     title: {
-      ru: 'ОБЩая 6 - МЕСТНая каюта',
-      en: 'Shared 6-bed cabin'
+      ru: '6-местный общий номер с с\\у и душевой',
+      en: '6-bed shared room with bathroom and shower'
     },
     content: {
-      ru: 'Номер для женщин с 6 местами, расположенными в комфортных двухъярусных «слип-боксах»',
-      en: 'Room for women with 6 seats, located in comfortable bunk " slip-boxes»'
+      ru: 'В номере расположены 3 двухъярусные кровати, письменные столы, уютный балкончик и персональные шкафчики, закрывающиеся на специальную карту.',
+      en: 'The room has 3 bunk beds, desks, a cozy balcony and personal lockers that can be closed with a special card.'
     },
     advantages: [
       {
@@ -115,12 +115,16 @@ export const roomsData = [
         en: 'shelf for small things'
       },
       {
-        ru: 'шкаф для одежды и ящик для багажа',
-        en: 'wardrobe and luggage storage box'
-      },
-      {
         ru: 'вход закрывается плотной шторкой',
         en: 'the entrance is closed with a thick curtain'
+      },
+      {
+        ru: '1 розетка обычная и 2 usb',
+        en: '1 regular socket and 2 usb'
+      },
+      {
+        ru: 'фен',
+        en: 'fan'
       }
     ],
     imageSet: [
@@ -132,8 +136,8 @@ export const roomsData = [
       }
     ],
     additionContent: [{
-      ru: 'К услугам гостей просторные общие душевые и туалеты, полностью укомплектованная кухня, зона отдыха с настольными играми и игровой консолью.',
-      en: 'It offers spacious shared showers and toilets, a fully equipped kitchen, and a seating area with board games and a game console.'
+      ru: 'В номере расположены 3 двухъярусные кровати, письменные столы, уютный балкончик и персональные шкафчики, закрывающиеся на специальную карту.',
+      en: 'The room has 3 bunk beds, desks, a cozy balcony and personal lockers that can be closed with a special card.'
     }]
   },
   // -------------
@@ -146,8 +150,8 @@ export const roomsData = [
       // avif: require('@/assets/images/room8/1.jpg?resize&format=avif')
     },
     title: {
-      ru: 'Шестиместный женский номер с собственным с\\у и душем',
-      en: 'Six-bed female room with private bathroom and shower'
+      ru: '6-местный женский номер с собственным с\\у и душем',
+      en: '6-bed female room with private bathroom and shower'
     },
     content: {
       ru: 'В номере расположены 3 двухъярусные кровати, письменные столы, уютный балкончик и персональные шкафчики, закрывающиеся на единую ключ-карту. В санузле предусмотрены фен и специальный стеллаж для мелочей.',
@@ -246,17 +250,17 @@ export const roomsData = [
   {
     slug: 'nine-room',
     imgSrc: {
-      jpg: require('@/assets/images/2room/r1.jpg')
+      jpg: require('@/assets/images/9room/r1 (1).jpg')
       // jpg: require('@/assets/images/room6/1.jpg?resize'),
       // webp: require('@/assets/images/room6/1.jpg?resize&format=webp'),
     },
     title: {
-      ru: 'ОБЩАЯ 10 - МЕСТНая каюта',
-      en: 'SHARED 10-BED cabin'
+      ru: '9-местный общий номер с с\\у и душевой',
+      en: '9-bed shared room with bathroom and shower'
     },
     content: {
-      ru: 'Номер для мужчин и женщин с 10 местами, расположенными в комфортных двухъярусных «слип-боксах»',
-      en: 'Room for men and women with 10 seats, located in comfortable bunk " slip-boxes»'
+      ru: 'В номере расположено 4 двухъярусные и 1 отдельная односпальная кровати, уютный балкончик и персональные шкафчики, закрывающиеся на единую ключ-карту.',
+      en: 'The room has 4 bunk beds and 1 separate single bed, a cozy balcony and personal lockers that can be locked with a single key card.'
     },
     advantages: [
       {
@@ -272,50 +276,59 @@ export const roomsData = [
         en: 'comfortable pillows and blankets'
       },
       {
-        ru: 'индивидуальное освещение + розетка',
-        en: 'individual lighting + power socket'
+        ru: 'индивидуальное освещение',
+        en: 'individual lighting'
       },
       {
         ru: 'полочка для мелочей',
         en: 'shelf for small things'
       },
       {
-        ru: 'шкаф для одежды и ящик для багажа',
-        en: 'wardrobe and luggage storage box'
+        ru: '2 розетки',
+        en: '2 sockets'
       },
       {
-        ru: 'закрывается плотной шторкой',
-        en: 'closes with a thick curtain'
+        ru: 'вход закрывается плотной шторкой',
+        en: 'the entrance is closed with a tight curtain'
       }
     ],
     imageSet: [
       {
-        jpg: require('@/assets/images/2room/r1.jpg')
+        jpg: require('@/assets/images/9room/r1 (1).jpg')
         // jpg: require('@/assets/images/room6/1.jpg?resize'),
         // webp: require('@/assets/images/room6/1.jpg?resize&format=webp'),
         // avif: require('@/assets/images/room6/1.jpg?resize&format=avif')
+      },
+      {
+        jpg: require('@/assets/images/9room/r1 (2).jpg')
+      },
+      {
+        jpg: require('@/assets/images/9room/r1 (3).jpg')
+      },
+      {
+        jpg: require('@/assets/images/9room/r1 (4).jpg')
       }
     ],
     additionContent: [{
-      ru: 'К услугам гостей просторные общие душевые и туалеты, полностью укомплектованная кухня, зона отдыха с настольными играми и игровой консолью.',
-      en: 'It offers spacious shared showers and toilets, a fully equipped kitchen, and a seating area with board games and a game console.'
+      ru: 'В номере расположено 4 двухъярусные и 1 отдельная односпальная кровати, уютный балкончик и персональные шкафчики, закрывающиеся на единую ключ-карту.',
+      en: 'The room has 4 bunk beds and 1 separate single bed, a cozy balcony and personal lockers that can be locked with a single key card.'
     }]
   },
   // -------------
   {
     slug: 'four-room',
     imgSrc: {
-      jpg: require('@/assets/images/2room/r1.jpg')
+      jpg: require('@/assets/images/4room/r1 (1).jpg')
       // jpg: require('@/assets/images/room6/1.jpg?resize'),
       // webp: require('@/assets/images/room6/1.jpg?resize&format=webp'),
     },
     title: {
-      ru: 'ОБЩая 12 - МЕСТНая каюта',
-      en: 'Shared 12-bed cabin'
+      ru: '4-местный общий номер',
+      en: '4-bed shared room'
     },
     content: {
-      ru: 'Номер для мужчин и женщин с 12 местами, расположенными в комфортных двухъярусных «слип-боксах»',
-      en: 'Room for men and women with 12 seats, located in comfortable bunk " slip-boxes»'
+      ru: 'В номере расположены 2 двухъярусные кровати, письменный стол, персональные шкафчики, закрывающиеся на единую ключ-карту. Общий санузел и душевая вне номера.',
+      en: 'The room has 2 bunk beds, a desk, personal lockers that can be locked with a single key card. Shared bathroom and shower outside the room.'
     },
     advantages: [
       {
@@ -339,8 +352,8 @@ export const roomsData = [
         en: 'shelf for small things'
       },
       {
-        ru: 'шкаф для одежды и ящик для багажа',
-        en: 'wardrobe and luggage storage box'
+        ru: '2 розетки',
+        en: '2 sockets'
       },
       {
         ru: 'вход закрывается плотной шторкой',
@@ -349,14 +362,29 @@ export const roomsData = [
     ],
     imageSet: [
       {
-        jpg: require('@/assets/images/2room/r1.jpg')
+        jpg: require('@/assets/images/4room/r1 (1).jpg')
         // jpg: require('@/assets/images/room6/1.jpg?resize'),
         // webp: require('@/assets/images/room6/1.jpg?resize&format=webp'),
+      },
+      {
+        jpg: require('@/assets/images/4room/r1 (2).jpg')
+      },
+      {
+        jpg: require('@/assets/images/4room/r1 (3).jpg')
+      },
+      {
+        jpg: require('@/assets/images/4room/r1 (4).jpg')
+      },
+      {
+        jpg: require('@/assets/images/4room/r1 (5).jpg')
+      },
+      {
+        jpg: require('@/assets/images/4room/r1 (6).jpg')
       }
     ],
     additionContent: [{
-      ru: 'К услугам гостей просторные общие душевые и туалеты, полностью укомплектованная кухня, зона отдыха с настольными играми и игровой консолью.',
-      en: 'It offers spacious shared showers and toilets, a fully equipped kitchen, and a seating area with board games and a game console.'
+      ru: 'В номере расположены 2 двухъярусные кровати, письменный стол, персональные шкафчики, закрывающиеся на единую ключ-карту. Общий санузел и душевая вне номера.',
+      en: 'The room has 2 bunk beds, a desk, personal lockers that can be locked with a single key card. Shared bathroom and shower outside the room.'
     }]
   }
 ]
