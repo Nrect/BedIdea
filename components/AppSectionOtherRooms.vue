f8fd57b05325f37f6dafb2f5db1f51104945280d
Лисицын Александр <bossstartov57@gmail.com>
2021-09-20 13:24:37 +0200

    fixes

--- components/AppSectionOtherRooms.vue
+++ components/AppSectionOtherRooms.vue
@@ -2,30 +2,6 @@
   .other-rooms-section.page-section
     .container
       h2.title {{sectionTitle}}
-        //.card-slider
-        //  hooper(ref="carousel" :settings="hooperSettings" @slide="checkSlideOption")
-        //    slide(v-for="(room,idx) in rooms" :key="idx").card-slider__slide
-        //      .room-card
-        //        .room-card__wrapper
-        //          .room-card__img
-        //            //picture
-        //            //  source(type="image/webp" :srcSet="room.imgSrc.webp.srcSet")
-        //            //  //- source(type="image/avif" :srcSet="room.imgSrc.avif.srcSet")
-        //            //  source(type="image/jpeg" :srcSet="room.imgSrc.jpg.srcSet")
-        //            img(:src="room.jpg")
-        //          h3.room-card__title {{room.title[actualLocale]}}
-        //          .room-card__description {{room.content[actualLocale]}}
-        //          span.room-card__btn.btn
-        //            nuxt-link(:to="localePath('/rooms/'+ room.slug)" ) {{ translate.learnMore }}
-        //.card-slider__buttons
-        //  button(@click.prevent="slidePrev" ref="prevBtn").card-slider__btn
-        //    span
-        //      svg(style="transform: rotate(180deg);")
-        //        use(xlink:href="#angle")
-        //  button(@click.prevent="slideNext" ref="nextBtn").card-slider__btn
-        //    span
-        //      svg
-        //        use(xlink:href="#angle")
       hooper.gallery__slider(
         ref="carousel"
         :settings="hooperSettings"
@@ -86,7 +62,7 @@ export default {
             itemsToShow: 2
           },
           556: {
-            itemsToShow: 2
+            itemsToShow: 1.5
           }
         }
       }
